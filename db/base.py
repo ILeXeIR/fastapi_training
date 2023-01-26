ebbedd77fa7247e9d234c31d562d4b58d5672e0a
ILeXeIR <al.v.petukhov@gmail.com>
2023-01-26 13:04:48 +0400

    Completed 1st lesson

--- db/base.py
+++ db/base.py
@@ -2,8 +2,8 @@ from databases import Database
 from sqlalchemy import create_engine, MetaData
 from core.config import DATABASE_URL
 
-database = Database(DATADASE_URL)
+database = Database(DATABASE_URL)
 metadata = MetaData()
 engine = create_engine(
-	DATADASE_URL,
+	DATABASE_URL,
 )
